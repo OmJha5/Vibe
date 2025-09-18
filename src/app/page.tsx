aeb3d848e7762af94b8a874aaed9a0cb2bc86d53
om <omjha003@gmail.com>
2025-09-18 14:14:36 +0530

    Integrated OpenAI into the project and verified its response in inngest dev server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+"use client"
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { useTRPC } from '@/trpc/client'
+import { useMutation } from '@tanstack/react-query';
+import React, { useState } from 'react'
+import { toast } from 'sonner';
 
 export default function page() {
+  let trpc = useTRPC();
+  
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess : () => {
+      toast.success("Background Job started..")
+    }
+  }));
+  let [code , setCode] = useState("");
+
   return (
-    <div className='text-red-700'>
-      Hello world
+    <div className='p-10 space-y-4'>
+      <Input value={code} onChange={(e) => setCode(e.target.value)}  />
+      <Button onClick={() => invoke.mutate({text : code})}>
+        Invoke Background Job
+      </Button>
     </div>
   )
 }
---
+++ src/inngest/functions.ts
@@ -0,0 +1,21 @@
+import { openai, createAgent } from "@inngest/agent-kit";
+import { inngest } from "./client";
+
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert next.js developer .  you write readable and maintanable code and write simple next.js and react snippets.",
+      model: openai({ model: "gpt-4o" }),
+    });
+
+    const { output } = await codeAgent.run(
+      `write the following snippet : ${event.data.email}`
+    );
+
+    return output;
+  },
+);
