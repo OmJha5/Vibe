8e0b035986dadd1b247c22d28d8b7cbf6e55565d
om <omjha003@gmail.com>
2025-09-18 19:14:37 +0530

    Setup the e2b sandbox , and tested the nextjs site by creating its sandbox container and got their public sandbox url

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,18 @@
+import {Sandbox} from "@e2b/code-interpreter"
 import { openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
 
-  async ({ event }) => {
+  async ({ event , step}) => {
+    const sandboxId = await step.run("get-sandbox-id" , async() => {
+      const sandbox = await Sandbox.create("vibe-nextjs-test-omkumarjha2");
+      return sandbox.sandboxId;
+    })
+
     const codeAgent = createAgent({
       name: "code-agent",
       system: "You are an expert next.js developer .  you write readable and maintanable code and write simple next.js and react snippets.",
@@ -16,6 +23,12 @@ export const helloWorld = inngest.createFunction(
       `write the following snippet : ${event.data.email}`
     );
 
-    return output;
+    const sandboxUrl = await step.run("get-sandbox-url" , async() => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000); // Take whatever is running inside the container on port 3000 and give me a publicly accessible URL for it.
+      return `https://${host}`
+    })
+
+    return {output , sandboxUrl};
   },
 );
